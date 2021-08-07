0539adb914931ee4264f0d3ce430f9a2ee91082a
Vladimir Rodchenko <vladimir@rodchenko.by>
2021-08-07 18:52:21 +0300

    add first test

---
+++ test/GodelTech.Auth.IdentityModel.Tests/ClientCredentialsFlowTokenServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Xunit;
+
+namespace GodelTech.Auth.IdentityModel.Tests
+{
+    public class ClientCredentialsFlowTokenServiceTests
+    {
+        private readonly ClientCredentialsFlowTokenOptions _clientCredentialsFlowTokenOptions;
+        private readonly Mock<IHttpClientFactory> _mockHttpClientFactory;
+        private readonly Mock<ILogger<ClientCredentialsFlowTokenService>> _mockLogger;
+
+        private readonly ClientCredentialsFlowTokenService _service;
+
+        public ClientCredentialsFlowTokenServiceTests()
+        {
+            _clientCredentialsFlowTokenOptions = new ClientCredentialsFlowTokenOptions
+            {
+                Authority = "Test Authority",
+                ClientId = "Test ClientId",
+                ClientSecret = "Test ClientSecret",
+                Scope = "Test Scope"
+            };
+
+            var mockClientCredentialsFlowTokenOptions = new Mock<IOptions<ClientCredentialsFlowTokenOptions>>(MockBehavior.Strict);
+            mockClientCredentialsFlowTokenOptions
+                .Setup(x => x.Value)
+                .Returns(_clientCredentialsFlowTokenOptions);
+
+            _mockLogger = new Mock<ILogger<ClientCredentialsFlowTokenService>>(MockBehavior.Strict);
+            _mockHttpClientFactory = new Mock<IHttpClientFactory>(MockBehavior.Strict);
+
+            _service = new ClientCredentialsFlowTokenService(
+                mockClientCredentialsFlowTokenOptions.Object,
+                _mockHttpClientFactory.Object,
+                _mockLogger.Object
+            );
+        }
+
+        [Fact]
+        public void Constructor_ThrowsArgumentNullException()
+        {
+            // Arrange & Act & Assert
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => new ClientCredentialsFlowTokenService(
+                    null,
+                    _mockHttpClientFactory.Object,
+                    _mockLogger.Object
+                )
+            );
+
+            Assert.Equal("clientCredentialsFlowTokenOptions", exception.ParamName);
+        }
+    }
+}
