854c95223142ef861f57d2deba5033d244ab9385
Vladimir Rodchenko <vladimir@rodchenko.by>
2021-08-07 17:16:49 +0300

    solve CLSCompliant warning

--- src/GodelTech.Auth.IdentityModel/ITokenService.cs
+++ src/GodelTech.Auth.IdentityModel/ITokenService.cs
@@ -1,6 +1,8 @@
-﻿using IdentityModel.Client;
+﻿using System;
+using IdentityModel.Client;
 using System.Threading.Tasks;
 
+[assembly: CLSCompliant(false)]
 namespace GodelTech.Auth.IdentityModel
 {
     /// <summary>
